51f1774052b78b720afdfbb810a8ba977a7cc035
Cyber Shield Team <117581968+CyberShieldDev@users.noreply.github.com>
2022-12-24 02:17:33 -0800

    Add files via upload

---
+++ Cyber_Shield_CTF_1.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main(){
+cout<<"Cyber Shield \n Reverse Engineering CTF \n level:Easy\n";
+int p=5;
+if(p==1){
+cout<<"win";
+}else{
+cout<<"lose";
+}
+return 0;
+}
---
+++ Cyber_Shield_CTF_2.cpp
@@ -0,0 +1,1268 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+int kalimera(int &one){
+		int apple=12;
+	for(int i=0;i<10;i++){
+		if(i==1){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==2){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==3){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==4){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==5){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==6){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==7){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==8){
+			for(int j=0;j<5;j++){
+					if(i==1){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==2){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==3){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==4){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==5){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+			}
+		}
+		if(i==9){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==10){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+	}
+	one=52;
+	return one;
+}
+int sulimany1110(int &two){
+		int apple=12;
+	for(int i=0;i<10;i++){
+		if(i==1){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==2){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==3){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==4){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==5){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==6){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==7){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==8){
+			for(int j=0;j<5;j++){
+					if(i==1){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==2){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==3){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==4){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==5){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+			}
+		}
+		if(i==9){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==10){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+	}
+	two=114;
+	return two;
+}
+int double_apple(int &three){
+		int apple=12;
+	for(int i=0;i<10;i++){
+		if(i==1){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==2){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==3){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==4){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==5){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==6){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==7){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==8){
+			for(int j=0;j<5;j++){
+					if(i==1){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==2){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==3){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==4){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==5){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+			}
+		}
+		if(i==9){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==10){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+	}
+	three=51;
+	return three;
+}
+int shwtiukalkutre121212(int &four){
+		int apple=12;
+	for(int i=0;i<10;i++){
+		if(i==1){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==2){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==3){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==4){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==5){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==6){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==7){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==8){
+			for(int j=0;j<5;j++){
+					if(i==1){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==2){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==3){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==4){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==5){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+			}
+		}
+		if(i==9){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==10){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+	}
+	four=90;
+	return four;
+}
+int linuxunixwindowsmacosios(int &five){
+		int apple=12;
+	for(int i=0;i<10;i++){
+		if(i==1){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==2){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==3){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==4){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==5){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==6){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==7){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==8){
+			for(int j=0;j<5;j++){
+					if(i==1){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==2){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==3){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==4){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==5){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+			}
+		}
+		if(i==9){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==10){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+	}
+	five=67;
+	return five;
+}
+int abcabcacbcacba212121212(int &six){
+		int apple=12;
+	for(int i=0;i<10;i++){
+		if(i==1){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==2){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==3){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==4){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==5){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==6){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==7){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==8){
+			for(int j=0;j<5;j++){
+					if(i==1){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==2){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==3){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==4){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==5){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+			}
+		}
+		if(i==9){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==10){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+	}
+	six=89;
+	return six;
+}
+int hhhhhashhhhh222333(int &seven){
+		int apple=12;
+	for(int i=0;i<10;i++){
+		if(i==1){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==2){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==3){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==4){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==5){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==6){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==7){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==8){
+			for(int j=0;j<5;j++){
+					if(i==1){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==2){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==3){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==4){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==5){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+			}
+		}
+		if(i==9){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==10){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+	}
+	seven=98;
+	return seven;
+}
+int hhhhhashhhhhhashh2222(int &eight){
+		int apple=12;
+	for(int i=0;i<10;i++){
+		if(i==1){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==2){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==3){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==4){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==5){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==6){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==7){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==8){
+			for(int j=0;j<5;j++){
+					if(i==1){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==2){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==3){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==4){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==5){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+			}
+		}
+		if(i==9){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==10){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+	}
+	eight=51;
+	return eight;
+}
+int jkljkljk32321kjll(int &nine){
+		int apple=12;
+	for(int i=0;i<10;i++){
+		if(i==1){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==2){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==3){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==4){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==5){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==6){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==7){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==8){
+			for(int j=0;j<5;j++){
+					if(i==1){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==2){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==3){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==4){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==5){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+			}
+		}
+		if(i==9){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==10){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+	}
+	nine=82;
+	return nine;
+}
+int asdasdj3sjk3(int &ten){
+		int apple=12;
+	for(int i=0;i<10;i++){
+		if(i==1){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==2){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==3){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==4){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==5){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==6){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==7){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==8){
+			for(int j=0;j<5;j++){
+					if(i==1){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==2){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==3){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==4){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==5){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+			}
+		}
+		if(i==9){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==10){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+	}
+	ten=48;
+	return ten;
+}
+int main(){
+	int apple=12;
+	for(int i=0;i<10;i++){
+		if(i==1){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==2){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==3){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==4){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==5){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==6){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==7){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==8){
+			for(int j=0;j<5;j++){
+					if(i==1){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==2){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==3){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==4){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==5){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+			}
+		}
+		if(i==9){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+		if(i==10){
+			for(int j=0;j<5;j++){
+				int sum=(apple+j*5+3+2)%4;
+				sum=sum*sum*sum;
+
+			}
+		}
+	}
+	string s;
+	int p;
+	for(int i=1;i<=10;i++){
+		if(i==1){
+			kalimera(p);
+			s+=char(p);
+		}
+		if(i==2){
+			sulimany1110(p);
+			s+=char(p);
+		}
+		if(i==3){
+			double_apple(p);
+			s+=char(p);
+		}
+		if(i==4){
+			shwtiukalkutre121212(p);
+			s+=char(p);
+		}
+		if(i==5){
+			linuxunixwindowsmacosios(p);
+			s+=char(p);
+		}
+		if(i==6){
+			abcabcacbcacba212121212(p);
+			s+=char(p);
+		}
+		if(i==7){
+			hhhhhashhhhh222333(p);
+			s+=char(p);
+		}
+		if(i==8){
+			hhhhhashhhhhhashh2222(p);
+			s+=char(p);
+		}
+		if(i==9){
+			jkljkljk32321kjll(p);
+			s+=char(p);
+		}
+		if(i==10){
+			asdasdj3sjk3(p);
+			s+=char(p);
+		}
+	}
+	cout<<"Cyber Shield \n Reverse Engineering CTF \n level:Normal\n";
+	string input;
+	cout<<"Enter Password: ";
+	cin>>input;
+	bool correct=false;
+	for(int i=0;i<10;i++){
+		if(input.length()>10||input.length()<10){
+			break;
+		}
+			if(s[i]==input[i]){
+				correct=true;
+			}else{
+				correct=false;
+				break;
+			}
+			if(i==9){
+			if(correct){
+				cout<<"\nWelcome";
+			}
+		}
+	}
+
+	return 0;
+}
